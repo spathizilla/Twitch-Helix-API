ab63ea08be42e3c993636961c5f510a66537d4bc
Chris Perez <chrisv.perez@gmail.com>
2020-03-29 15:43:56 -0400

    first commit
    - updated authentication api usage
    - added rate limiting
    - added clips
    - replaced usage of var
    - added gitignore

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-exports.clientID =  "";
+exports.clientID = "";
+exports.clientSecret = "";
+exports.token = "";
 exports.authentication = require("./lib/authentication");
 exports.games = require("./lib/games");
 exports.streams = require("./lib/streams");
 exports.users = require("./lib/users");
 exports.videos = require("./lib/videos");
+exports.clips = require("./lib/clips");
